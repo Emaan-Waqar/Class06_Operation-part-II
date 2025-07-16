728678e314a369fdee7370328574e94abb0884d7
Emaan Waqar <emaanwaqar0102@gmail.com>
2025-07-16 22:54:15 +0500

    Assignment

---
+++ assignment.py
@@ -0,0 +1,58 @@
+character= input("Enter a character: ")
+character= character.lower()
+if len(character) !=1:
+    print("Enter one character only")
+elif character == "a":
+    print(character,"is an alphabet")
+elif character == "b":
+    print(character,"is an alphabet")
+elif character == "c":
+    print(character,"is an alphabet")
+elif character == "d":
+    print(character,"is an alphabet")    
+elif character == "e":
+    print(character,"is an alphabet")
+elif character == "f":
+    print(character,"is an alphabet")
+elif character == "g":
+    print(character,"is an alphabet")        
+elif character == "h":
+    print(character,"is an alphabet")            
+elif character == "i":
+    print(character,"is an alphabet")   
+elif character == "j":
+    print(character,"is an alphabet") 
+elif character == "k":
+    print(character,"is an alphabet")    
+elif character == "l":
+    print(character,"is an alphabet")
+elif character == "m":
+    print(character,"is an alphabet")
+elif character == "n":
+    print(character,"is an alphabet")
+elif character == "o":
+    print(character,"is an alphabet")
+elif character == "p":
+    print(character,"is an alphabet")
+elif character == "q":
+    print(character,"is an alphabet")    
+elif character == "r":
+    print(character,"is an alphabet")
+elif character == "s":
+    print(character,"is an alphabet")
+elif character == "t":
+    print(character,"is an alphabet")
+elif character == "u":
+    print(character,"is an alphabet")   
+elif character == "v":
+    print(character,"is an alphabet") 
+elif character == "w":
+    print(character,"is an alphabet")
+elif character == "x":
+    print(character,"is an alphabet")
+elif character == "y":
+    print(character,"is an alphabet")
+elif character == "z":
+    print(character,"is an alphabet")
+else:
+    print(character, "is not an alphabet")    
