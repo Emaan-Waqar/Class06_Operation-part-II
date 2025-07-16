5d4bf7b4689e6746593cb6eb371b95255dcb13d6
Emaan Waqar <emaanwaqar0102@gmail.com>
2025-07-16 12:53:10 +0500

    And Operator

---
+++ andOperator.py
@@ -0,0 +1,23 @@
+a=10
+b=12
+c=0
+if a > c and b > c and a > b:
+    print("All the numbers have boolean value True")
+else: 
+    print("Atleast one number has boolean value as False")   
+
+
+
+
+a=10
+b=-10
+c=0
+if a > 0 or b > 0:
+    print("Either of the number is greater than 0")
+else:
+    print("No number is greater than zero")    
+
+if b > 0 or c > 0:
+    print("One of the conditions is true")
+else:
+    print("No number is greater than 0")        
